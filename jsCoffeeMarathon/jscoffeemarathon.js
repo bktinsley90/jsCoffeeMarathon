c53d4ab5af467b3db504866210364153755d3405
Brittany Tinsley <brittanytinsley@Brittanys-MacBook-Pro.local>
2020-03-07 14:34:02 -0500

    adding first commit

---
+++ jsCoffeeMarathon/jscoffeemarathon.js
@@ -0,0 +1,27 @@
+console.log ("Coffee Please!")
+let coffeeType = "Medium Roast";
+console.log (coffeeType);
+var latte = 2;
+var mocha = 3;
+console.log (latte + mocha)
+console.log (latte > mocha)
+console.log(latte<mocha)
+
+let sugarFree;
+alert (sugarFree)
+
+let customer1 = "Khamala Khan", drinkchoice = "Chai", quanity = 4, foodAllergy = false;
+console.log(`${customer1} ${drinkchoice} ${quanity} ${foodAllergy}`)
+let chai = 2.50;
+console.log(chai*quanity)
+
+chai = 2;
+console.log(chai) 
+
+console.log("+++++++++++++++++++++++++");
+let total = `Your Final Order is ${drinkchoice} Quanity:${quanity} total:${quanity*chai}`
+
+alert (total);
+console.log("+++++++++++++++++++++++++");
+
+console.log("Nice choice of drinks , but we recommend Sprite")
